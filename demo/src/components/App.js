c70316d19472dbb600fed9ab8c612ca90587a3fe
Matt Werdean <mwerdean@yahoo.com>
2017-12-12 11:23:35 -0700

    step uno

--- demo/src/components/App.js
+++ demo/src/components/App.js
@@ -1,10 +1,19 @@
 import React, { Component } from 'react';
+// import { Route } from '../../../../../../AppData/Local/Microsoft/TypeScript/2.6/node_modules/@types/react-router';
+import {Route, Link} from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
       <div>
-        This is App.js
+        <div>
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+        <Link to="/people">People</Link>
+      </div>
+        <Route exact path="/" render={() => <div>Hello</div>}/>
+        <Route path="/about" render={() => <div>About</div>} />
+        <Route path="/people" render = {() => <div>People</div>} />
       </div>
     );
   }
