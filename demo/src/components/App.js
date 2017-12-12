2c9c493d1866d63e0058541165d1b9c4d2e3b8d7
Matt Werdean <mwerdean@yahoo.com>
2017-12-12 15:18:35 -0700

    completed mini project

--- demo/src/components/App.js
+++ demo/src/components/App.js
@@ -1,6 +1,13 @@
 import React, { Component } from 'react';
 // import { Route } from '../../../../../../AppData/Local/Microsoft/TypeScript/2.6/node_modules/@types/react-router';
-import {Route, Link} from 'react-router-dom';
+import {Route, Link, Switch} from 'react-router-dom';
+import Home from './Home/Home.js';
+import People from './People/People.js';
+import About from './About/About.js';
+import FAQ from './About/FAQ/FAQ';
+import Company from './About/Company/Company';
+import Character from './Character/Character';
+// import { Switch } from '../../../../../../AppData/Local/Microsoft/TypeScript/2.6/node_modules/@types/react-router';
 
 class App extends Component {
   render() {
@@ -11,9 +18,20 @@ class App extends Component {
         <Link to="/about">About</Link>
         <Link to="/people">People</Link>
       </div>
-        <Route exact path="/" render={() => <div>Hello</div>}/>
+        {/* <Route exact path="/" render={() => <div>Hello</div>}/>
         <Route path="/about" render={() => <div>About</div>} />
-        <Route path="/people" render = {() => <div>People</div>} />
+        <Route path="/people" render = {() => <div>People</div>} /> */}
+        <Route exact path="/" component={Home}/>
+        <Route path="/about" render={() => 
+          <About>
+            <Route path ="/about/faq" component={FAQ}/>
+            <Route path ="/about/company" component={Company}/>
+          </About>
+        }/>
+        <Switch>
+          <Route path="/people/:id" component={Character}/> 
+          <Route path="/people" component={People}/>
+        </Switch>
       </div>
     );
   }
